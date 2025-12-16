5b3a4e96d8c377cddddf3df940d6959c57865647
thienhd <163114540+hoangdangthien@users.noreply.github.com>
2025-12-16 10:24:30 +0700

    Add files via upload

--- rxconfig.py
+++ rxconfig.py
@@ -1,4 +1,26 @@
 import reflex as rx
+import urllib
+import os
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
+
+# MSSQL Connection Configuration from environment
+server = os.getenv('MSSQL_SERVER', '')
+database = os.getenv('MSSQL_NAME', '')
+username = os.getenv('MSSQL_USER', '')
+password = os.getenv('MSSQL_PASSWORD', '')
+driver = os.getenv('MSSQL_DRIVER', 'ODBC Driver 17 for SQL Server')
+
+# Build connection string
+conn = f"""Driver={{{driver}}};Server={server};Database={database};Uid={username};Pwd={password};Encrypt=yes;TrustServerCertificate=yes;"""
+
+# URL encode the connection string
+params = urllib.parse.quote_plus(conn)
+
+# Create connection string with autocommit
+db_url = f'mssql+pyodbc:///?autocommit=true&charset=utf8mb4&odbc_connect={params}'
 
 config = rx.Config(
     app_name="production",
@@ -6,5 +28,14 @@ config = rx.Config(
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
     ],
-    db_url="sqlite:///Production.db",
+    db_url=db_url,
 )
+
+"""config = rx.Config(
+    app_name="production",
+    plugins=[
+        rx.plugins.SitemapPlugin(),
+        rx.plugins.TailwindV4Plugin(),
+    ],
+    db_url="sqlite:///Production.db",
+)"""
